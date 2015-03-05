6a3dbe0add506f34938ab92ba7f49f89e01b122a
Michael Howard <mahoward@ucsc.edu>
2015-03-04 16:13:48 -0800

    Radix JS done

--- JS/radix.js
+++ JS/radix.js
@@ -1,60 +1,50 @@
-function radix(A,len){
-  ListRef buckets[10];
-  for(long i = 0; i < 10; i++){
-    buckets[i] = newList();
-  }
+function radix(A){
+  var buckets = [[],[],[],[],[],[],[],[],[],[]];
+  var max = 0;
+  var n = 1;
+  var base = 10;
+  var digit = 0;
 
-  long max = 0;
-  long n = 1;
-  long base = 10;
-  long digit;
-
-  for (long i = 1; i < len; i++) {
-      if (A[i] > max)
-        max = A[i];
+  for (var i = 1; i < A.length; i++) {
+      if (A[i] > max){max = A[i];}
   }
 
   while((max / n) > 0){
-    for(long i = 0; i < len; i++){
-      //printf("%ld ", A[i]);
+    for(var i = 0; i < A.length; i++){
       digit = A[i] % base;
-      digit = digit/n;
-      insertAfterLast(buckets[digit], A[i]);
+      //console.log(digit);
+      digit = Math.floor(digit / n);
+      //console.log(digit);
+      //console.log(buckets[digit]);
+      buckets[digit].push(A[i]);
     }
 
-    long index = 0;
-    for(long i = 0; i < 10; i++){
-      if(!isEmpty(buckets[i])){
-        while(!isEmpty(buckets[i])){
-          A[index] = getFirst(buckets[i]);
+    var index = 0;
+    for(var i = 0; i < 10; i++){
+      if(buckets[i].length != 0){
+        while(buckets[i].length != 0){
+          A[index] = buckets[i].shift();
           index++;
-          deleteFirst(buckets[i]);
         }
       }
     }
-
-    printf("\n");
-
-    for(long i = 0; i < len; i++){
-      printf("%ld ", A[i]);
-    }
-    printf("\n");
     base *= 10;
     n *= 10;
   }
-  return 0;
+  return A;
 }
-
 
 if (process.argv.length != 3) {
   process.exit(0);
 }
-var fs = require('fs'); // Read values from file
+var fs = require('fs');
 var array = fs.readFileSync(process.argv[2]).toString().split(/\s+/);
+
 for (var i = 0; i < array.length; ++i) {
   array[i] = parseInt(array[i]);
 }
-mergeSort(array); // mergesort
+
+radix(array);
 for (var i = 0; i < array.length; ++i) {
   console.log(array[i]);
 }
